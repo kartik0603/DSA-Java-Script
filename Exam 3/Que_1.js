c0eba608e85447d5e875915540800702d4a53387
kartik0603 <kartikhirapara800@gmail.com>
2024-08-20 09:54:05 +0530

    EXAM 3

---
+++ Exam 3/Que_1.js
@@ -0,0 +1,36 @@
+//  Check if Two Strings Are Anagrams 
+// Write a function to determine if two strings are anagrams of each other. Two strings are anagrams if
+// they contain the same characters in the same frequency but arranged differently.
+
+// Input: str1 = 'listen', str2 = 'silent' - Output: True
+
+
+
+const anagramStr =(str1,str2)=>{
+    if(str1.length !== str2.length){
+        return false;
+    }
+    // convert string to array
+
+    let array1 = str1.split('');
+    let array2 = str2.split('');
+
+    // sort  array
+
+    array1.sort();
+    array2.sort();
+
+    // join  array
+
+    let newStr1 = array1.join('');
+    let newStr2 = array2.join('');
+
+  
+
+    if(newStr1 === newStr2){
+        
+            return true;
+    }
+    return false;
+}
+console.log(anagramStr('listen','silent'));
